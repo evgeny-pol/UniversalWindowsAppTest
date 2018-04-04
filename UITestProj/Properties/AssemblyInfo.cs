162798aa59251bf2a4c857c2588cb06eab28dea1
Евгений Полетайкин <evgeniy.poletaikin@gmail.com>
2018-04-04 21:56:36 +0300

    add ui tests

---
+++ UITestProj/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UITestProj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UITestProj")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c222cefc-3e55-41bb-bfb6-2a921d695891")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UITestProj/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Appium.iOS;
+using OpenQA.Selenium.Remote;
+
+namespace UITestProj
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string WindowsApplicationDriverUrl = @"http://127.0.0.1:4723";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("app", "93011e2b-f3e0-4b2c-9307-fe76414c4d3e_e4thgbjj8qbka!App");
+            //using (var appSession = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities))
+            using (var appSession = new IOSDriver<IOSElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities))
+            {
+                var button1Elem = appSession.FindElementByAccessibilityId("Button1") as IOSElement;
+                Assert.IsNotNull(button1Elem);
+                button1Elem.Click();
+                var textBox1Elem = appSession.FindElementByAccessibilityId("TextBox1") as IOSElement;
+                Assert.IsNotNull(textBox1Elem);
+                Assert.AreEqual("Hello World!", textBox1Elem.Text);
+            }
+        }
+    }
+}
